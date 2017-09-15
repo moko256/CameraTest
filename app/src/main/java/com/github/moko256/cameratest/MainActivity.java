bf1627d1e5b8a3763e9d1043eaafd1475d603163
moko256 <koutaro.mo@gmail.com>
2017-09-16 00:45:23 +0900

    add camera intent

--- app/src/main/java/com/github/moko256/cameratest/MainActivity.java
+++ app/src/main/java/com/github/moko256/cameratest/MainActivity.java
@@ -1,8 +1,12 @@
 package com.github.moko256.cameratest;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchCamera(View view) {
+        startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), 0);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0 && resultCode == RESULT_OK && data != null){
+            ((ImageView) findViewById(R.id.imageView)).setImageBitmap(((Bitmap) data.getExtras().get("data")));
+        }
     }
 }
